0aaaca45361afae59b3b40408ac86b052fe48010
dennis_kofi_smith <dennissmithkofi@gmail.com>
2026-01-16 13:54:36 +0000

    setting up routes and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import './globals.css'
+import SignInForm from './_auth/forms/SignInForm';
+import { Home } from './_root/pages';
+import SignUpForm from './_auth/forms/SignUpForm';
 
 const App = () => {
   return (
-    <div>App</div>
+   <main className='flex h-screen'>
+    <Routes>
+      {/*  public routes - sign out or sign in  */}
+      <Route path='/sign-in' element={<SignInForm/>} />
+      <Route path='/sign-up' element={<SignUpForm/>} />
+
+      {/* private routes - only signed in users can see */}
+      <Route index element={<Home/>} />
+
+    </Routes>
+   </main>
   )
 }
 
